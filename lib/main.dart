5f3657de88982070181d6c8052edcef5e56563a9
Teltoriy <125376553+Teltoriy@users.noreply.github.com>
2024-09-15 18:31:15 +0300

    commit

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ЭФБО-01-22',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const SizedBox(height: 80),
+            const Center(
+              child: Text(
+                'Авторизация', //Игнатов Иван Алексеевич
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+            ),
+            // Container(
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(child: const SizedBox(height: 40)),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Логин',
+                filled: true,
+                fillColor: Colors.black12,
+                border: InputBorder.none,
+              ),
+            ),
+            const SizedBox(height: 16),
+            const TextField(
+              decoration: InputDecoration(
+                hintText: 'Пароль',
+                filled: true,
+                fillColor: Colors.black12,
+                border: InputBorder.none,
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 8),
+            Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Checkbox(value: false, onChanged: (bool? value) {}),
+                  const Text(
+                    'Запомнить меня',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Войти', style: TextStyle( fontSize: 18)),
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 16),
+                backgroundColor: Colors.blue,
+                foregroundColor: Colors.white,
+                padding: const EdgeInsets.all(15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(6.0),
+              child: const SizedBox(height: 8),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              style: OutlinedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.blue,
+                minimumSize: const Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                side: const BorderSide(
+                  color: Colors.blue,
+                  width: 1,
+                ),
+              ),
+              child: const Text('Регистрация', style: TextStyle( fontSize: 18)),
+            ),
+            const SizedBox(height: 8),
+            Center(
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Восстановить пароль',
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+
+        ),
+      ),
+    );
+  }
+}
